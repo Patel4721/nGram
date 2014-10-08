c377214a025ba4385298a4e597c0d9102392b5f2
Brian Perron <beperron@umich.edu>
2014-10-08 18:32:49 -0400

    More general cleanup.

--- ui.r
+++ ui.r
@@ -1,36 +1,40 @@
 library(shiny)
 
 shinyUI(navbarPage("Navbar!",
-                   tabPanel("Ngram Sentence Builder",
-                            sidebarPanel(
-                             
-                              helpText("A lot of data is being loaded ... Please be patient!"),
-                              textInput("grams", "Enter one or two words"),
-                             
-                              
-                              submitButton("Predict") 
-                              
-                            ),
+    
+      tabPanel("Overview", dataTableOutput("table")),
+                   
+                   
+      tabPanel("Ngram Sentence Builder",
+            sidebarPanel(
+                  helpText("Just a moment"),
+                  textInput("grams", "Enter one or two words"),
+                  submitButton("Predict")),
                         
-                            mainPanel(
-                              
-                              h4("Twitter Prediction"),
-                              textOutput("twitterSentence"),
-                              
-                              h4("News Prediction"),
-                              textOutput("newsSentence"),
-                              
-                              h4("Tostoy Prediction"),
-                              textOutput("tolstoySentence")
-                              )),
+              mainPanel(
+                  h4("Twitter Prediction"),
+                      textOutput("twitterSentence"),
+                  h4("News Prediction"),
+                       textOutput("newsSentence"),
+                  h4("Tostoy Prediction"),
+                        textOutput("tolstoySentence"))),
  
 
-                   tabPanel("Word Clouds",
-                            mainPanel(
-                              plotOutput("twitterCloud"),
-                              plotOutput("newsCloud"),
-                              plotOutput("tolstoyCloud")
-                              )),
-                   
-                   navbarMenu("More", tabPanel("Table", dataTableOutput("table")))
+       tabPanel("Word Clouds",
+              sidebarPanel(
+                  
+                helpText("A lot of data is being loaded ... Please be patient!"),
+                  
+                
+                sliderInput("numWords", 
+                            label = "Number of Words",
+                            min = 0, max = 100, value = 50)),
+                
+              mainPanel(
+                    plotOutput("twitterCloud"),
+                    plotOutput("newsCloud"),
+                    plotOutput("tolstoyCloud")
+                    ))
+      
+      
 ))
