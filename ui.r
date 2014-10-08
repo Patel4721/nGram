e80245d562200975ccbebd562d2cf3734a37ba64
Brian Perron <beperron@umich.edu>
2014-10-08 13:31:25 -0400

    Minor adjustment to filename that caused error on ShinyApp.io

--- ui.r
+++ ui.r
@@ -1,9 +1,5 @@
 library(shiny)
 
-#QUESTION:  I WANT TO HAVE A LAYOUT WITH A HORIZONTAL LINE DIVIDING THE SCREEN.  THE TOP 
-#SECTION WILL ACCEPT THE USER INPUT, AND THE BOTTOM SECTION WILL REPORT THE PREDICTED SENTENCES.
-
-
 shinyUI(navbarPage("Navbar!",
                    tabPanel("Ngram Sentence Builder",
                             sidebarPanel(
@@ -28,12 +24,6 @@ shinyUI(navbarPage("Navbar!",
                               textOutput("tolstoySentence")
                               )),
  
-#QUESTION: IN THIS SECTION, I WANT TO HAVE THREE WORD CLOUDS THAT ARE ALL CONTROLLED BY A SINGLE
-#SET OF USER INPUTS.  THE USER INPUTS WILL BE SLIDERS, CONTROLLING THE MINIMUM FREQUENCY
-#AND MAXIMUM NUMBER OF WORDS.  
-
-#NOTE: I AM GOING TO CREATE DIFFERENT DATA FILES FOR THE WORD CLOUDS THAN THE PREDICITON FILES.  
-
 
                    tabPanel("Word Clouds",
                             mainPanel(
