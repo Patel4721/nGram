2f99ef5a3d733fa3bf48b33cf038aeb758b4e405
Brian Perron <beperron@umich.edu>
2014-10-08 13:14:40 -0400

    Removed some files.

--- tolstoyBigramBackoff.r
+++
@@ -1,9 +0,0 @@
-tolstoyBi.f <- function(x){
-  lower.x <- tolower(x)
-  a <- grep(paste0(lower.x, '$'), tolstoyBigram.df[,"token.1"]) 
-  bigram.a <- tolstoyBigram.df[a,] # Subset data
-  bigram.a.ordered <- bigram.a[with(bigram.a, order(-biCount)),]
-  b <- as.character(bigram.a$token.2[1]) #select bigram
-  top.unigrams <- tolstoyUnigram.df[1:5, 2]
-  ifelse(is.na(b), return(sample(top.unigrams, 1)), return(b))
-}
--- tolstoyTrigramBackoff.r
+++
@@ -1,9 +0,0 @@
-tolstoyTri.f <- function (x){
-  lower.x <- tolower(x)
-  c <- colsplit(lower.x, " ", c("t1", "t2"))  
-  a <- grep(paste0(lower.x, '$'), tolstoyTrigram.df[,"token.1.2"])       
-  trigram.a <- tolstoyTrigram.df[a,] # Subset data
-  trigram.a.ordered <- trigram.a[with(trigram.a, order(-triCount)),]
-  ifelse(length(trigram.a.ordered$triCount) == 0, return(tolstoyBi.f(c$t2)), 
-         return(as.character(trigram.a.ordered$token.3[1])))
-}
